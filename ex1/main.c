2be9dcebcfe160cc2e03dfe0b8e6854ab9869bfd
ben6066 <ben6066@gmail.com>
2021-03-29 23:55:25 +0300

    updated cd command exit left

--- ex1/main.c
+++ ex1/main.c
@@ -4,6 +4,7 @@
 #include <sys/wait.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 typedef struct commandInfo {
     char command[100];
@@ -13,6 +14,7 @@ typedef struct commandInfo {
 } commandInfo;
 
 commandInfo commandsInfo[100];
+char oldPath[100];
 
 void commandInfoInit(commandInfo* command, char* userInput){
     // Initialize argv with NULLs
@@ -24,9 +26,11 @@ void commandInfoInit(commandInfo* command, char* userInput){
     // Copy the input from userInput to the struct
     strcpy(command->command, userInput);
 
-
     // Initialize isBackground
     command->isBackground = 0;
+
+    // Initialize pid
+    command->pid = -1;
 
     /*
      * Insert NULLs instead of every space character
@@ -58,7 +62,7 @@ void commandInfoInit(commandInfo* command, char* userInput){
         command->argv[argvIndex] = p1;
         argvIndex++;
         p1 = p2;
-        p2 = p1 + strlen(command->command) + 1;
+        p2 = p1 + strlen(p1) + 1;
     }
 
     // If the last argument is '&' - it's background
@@ -68,13 +72,174 @@ void commandInfoInit(commandInfo* command, char* userInput){
     }
 }
 
-void jobs(commandInfo* commandsInfo, int numberOfCommands){
-    
+void jobs(int numberOfCommands){
+    pid_t waitResult;
+    int argvIndex;
+
+    // Iterate over all of the commands
+    for(int i = 0; i < numberOfCommands; i++){
+        // Case the current command isn't a background command - skip it
+        if(commandsInfo[i].isBackground == 0)
+            continue;
+
+        argvIndex = 0;
+
+        // Check using WNOHANG if the background command is still running
+        waitResult = waitpid(commandsInfo[i].pid, NULL, WNOHANG);
+
+        // Case 'waitpid' returned 0 - the background command is still running
+        if(waitResult == 0){
+            // Print the command name
+            printf("%s", commandsInfo[i].argv[0]);
+            fflush(stdout);
+
+            argvIndex = 1;
+
+            // Print the command arguments
+            while(commandsInfo[i].argv[argvIndex] != NULL){
+                printf(" %s", commandsInfo[i].argv[argvIndex]);
+                fflush(stdout);
+                argvIndex++;
+            }
+
+            printf("\n");
+        }
+    }
+}
+
+void history(int numberOfCommands){
+    pid_t waitResult;
+    int argvIndex;
+    char* currentCommand;
+
+    // Iterate over all of the commands
+    for(int i = 0; i < numberOfCommands; i++){
+        currentCommand = commandsInfo[i].command;
+
+        argvIndex = 0;
+
+        // Case the current command is a 'built-in' command - mark as 'DONE'
+        if(strcmp(currentCommand, "jobs") == 0 || strcmp(currentCommand, "cd") == 0
+            || strcmp(currentCommand, "exit") == 0 || (strcmp(currentCommand, "history") == 0 && i != numberOfCommands - 1)){
+            waitResult = 1;
+        }
+
+       // Check using WNOHANG if the command is still running
+       else{
+            waitResult = waitpid(commandsInfo[i].pid, NULL, WNOHANG);
+       }
+
+        // Print the command name
+        printf("%s", commandsInfo[i].argv[0]);
+        fflush(stdout);
+
+        argvIndex = 1;
+
+        // Print the command arguments
+        while(commandsInfo[i].argv[argvIndex] != NULL){
+            printf(" %s", commandsInfo[i].argv[argvIndex]);
+            fflush(stdout);
+            argvIndex++;
+        }
+
+        // Case 'waitpid' returned 0 - the command is still running
+        if(waitResult == 0)
+            printf(" RUNNING\n");
+
+        // Otherwise, the command is done
+        else
+            printf(" DONE\n");
+    }
+
+    // The current command is history and of course it's still running
+    printf("history RUNNING\n");
+}
+
+void changeDirectory(int chdirIndex){
+    char* cdParam = commandsInfo[chdirIndex].argv[1];
+
+    // Case 'cd' was inserted with more than one argument - print error
+    if(commandsInfo[chdirIndex].argv[2] != NULL){
+        printf("Too many argument\n");
+        return;
+    }
+
+    int status;
+    char* homePath = getenv("HOME");
+    char currentPath[100];
+    memset(currentPath, NULL, 100);
+
+    // Save the current Path
+    getcwd(currentPath, 100);
+
+    // Case 'cd' has one argument or 'cd ~' was inserted - return home
+    if(cdParam == NULL || strcmp("~", cdParam) == 0){
+        status = chdir(homePath);
+        if(status == -1)
+            printf("An error occurred\n");
+        else
+            // Update the old path
+            strcpy(oldPath, currentPath);
+        return;
+    }
+
+    char destPath[100];
+    memset(destPath, NULL, 100);
+    int index;
+
+    // Case the path start with a '~'
+    if(cdParam[0] == '~') {
+        // Replace '~' with home path
+        strcpy(destPath, homePath);
+
+        index = strlen(homePath);
+
+        // Concatenates the path after the home path
+       for(int i = 1; i < strlen(cdParam); i++){
+           destPath[index] = cdParam[i];
+            index++;
+        }
+
+        // Change dir
+        status = chdir(destPath);
+    }
+
+    // Case the path start with a '-'
+    else if(cdParam[0] == '-'){
+        // Case the command wasn't 'cd -' it's not valid
+        if(cdParam[1] != '\0'){
+            printf("An error occurred\n");
+            return;
+        }
+
+        // Case it's the first time cd is used - no old path
+        if(oldPath[0] == '\0'){
+            printf("An error occurred\n");
+            return;
+        }
+
+        status = chdir(oldPath);
+    }
+
+    // Any other case
+    else{
+        status = chdir(cdParam);
+    }
+
+    // Case chdir failed
+    if(status == -1)
+        printf("chdir failed\n");
+
+    // Case chdir haven't failed - save the old path
+    else
+        // Update the old path
+        strcpy(oldPath, currentPath);
 }
 
 int main() {
     char userInput[100];
     int commandIndex = 0;
+    memset(oldPath, NULL, 100);
 
     while(1){
         // Display Prompt
@@ -94,7 +259,22 @@ int main() {
 
         // jobs command
         if(strcmp("jobs", userInput) == 0){
-            //jobs(commandsInfo, commandIndex);
+            jobs(commandIndex);
+            commandIndex++;
+            continue;
+        }
+
+        // history command
+        if(strcmp("history", userInput) == 0){
+            history(commandIndex);
+            commandIndex++;
+            continue;
+        }
+
+        // cd command
+        if(strcmp("cd", commandsInfo[commandIndex].command) == 0){
+            changeDirectory(commandIndex);
+            commandIndex++;
             continue;
         }
 
@@ -118,12 +298,10 @@ int main() {
 
         // Father
         else {
-            // Background
+            // Foreground
             if (commandsInfo[commandIndex].isBackground == 0){
                 waitResult = waitpid(commandsInfo[commandIndex].pid, NULL, 0);
-                printf("Wait: %d\n", waitResult);
             }
-            printf("Pid: %d\n", pid);
         }
 
         commandIndex++;
